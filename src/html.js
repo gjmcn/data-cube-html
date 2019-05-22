0536269025324c8e681a8f466cc8b9001e4acefb
gmcn <gjmcn@users.noreply.github.com>
2019-05-22 21:01:00 +0200

    remove comments

--- src/html.js
+++ src/html.js
@@ -427,18 +427,13 @@
     h = assert.single(h);
     const canvas = this.length ? this.slice(0,1).insert('canvas') : qa.create('canvas'),
           canvasElm = canvas[0];
-
-    // if (w !== undefined) canvasElm.setAttribute('width',  w);
-    // if (h !== undefined) canvasElm.setAttribute('height', h);
-    if (w !== undefined) canvasElm.style.width = w + 'px';
+    if (w !== undefined) canvasElm.style.width  = w + 'px';
     if (h !== undefined) canvasElm.style.height = h + 'px';
-    
     const ctx = canvasElm.getContext('2d');
     ctx.loop = Array.prototype.loop.bind([ctx]);
     return [canvas, ctx];
   });
 
-  
   module.exports = qa;
 
 })();
